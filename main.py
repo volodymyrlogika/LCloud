a851dd16929d249ece39dfd25dc6f1aa7c9dbe2f
volodymyrlogika <volodymyr.logika@gmail.com>
2024-09-19 19:09:39 +0300

    lesson1

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
+from kivy.lang  import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
 
 
-class MainApp(MDApp):
+class WeatherScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class LCloudApp(MDApp):
     def build(self):
+        Builder.load_file('style.kv')
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Purple"
 
-        return (
-            MDScreen(
-                MDRectangleFlatButton(
-                    text="Hello, World",
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                )
-            )
-        )
+        return WeatherScreen()
 
 
-MainApp().run()
+LCloudApp().run()
