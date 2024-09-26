0d935fe11888f1d895a9971e8bdcb3dd52f29c08
volodymyrlogika <volodymyr.logika@gmail.com>
2024-09-26 20:29:35 +0300

    додали пошук погоди

--- main.py
+++ main.py
@@ -2,11 +2,33 @@ from kivy.lang  import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
+import requests
+from settings import *
 
 
 class WeatherScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+    def get_weather(self, city):
+        params = {
+            "q": city,
+            "appid": API_KEY,
+        }
+        data = requests.get(CURRENT_WEATHER_URL, params)
+        response = data.json()
+        print(response)
+        return response
+    
+    def search(self):
+        city = self.ids.city.text
+        weather = self.get_weather(city)
+        
+        temp = weather["main"]["temp"]
+        self.ids.temp.text = f"{round(temp)}°C"
+        
+
 
 
 class LCloudApp(MDApp):
