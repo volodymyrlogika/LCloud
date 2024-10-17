ca9b6d1600d8660da3f69f03ffb20d0b035235f0
volodymyrlogika <volodymyr.logika@gmail.com>
2024-10-17 21:04:21 +0300

    last lesson

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from kivy.lang  import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.card import MDCard
+
 import requests
 from settings import *
 
@@ -9,7 +12,7 @@ from settings import *
 class WeatherScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
+        self.city = 'Львів'
 
     def get_weather(self, city):
         params = {
@@ -22,8 +25,8 @@ class WeatherScreen(MDScreen):
         return response
     
     def search(self):
-        city = self.ids.city.text
-        weather = self.get_weather(city)
+        self.city = self.ids.city.text
+        weather = self.get_weather(self.city)
         # додаємо температуру у вікно
         temp = weather["main"]["temp"]
         self.ids.temp.text = f"{round(temp)}°C"
@@ -43,6 +46,48 @@ class WeatherScreen(MDScreen):
         icon = weather["weather"][0]["icon"]
         self.ids.icon.source = f'https://openweathermap.org/img/wn/{icon}@2x.png'
 
+    def show_forecast(self):
+        forecast_data = self.forecast.get_forecast(self.city)
+        self.forecast.show_forecast(forecast_data)
+        self.manager.transition.direction = 'left'
+        self.manager.current = 'forecast'
+        
+
+class WeatherCard(MDCard):
+    def __init__(self, weather, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        temp = weather["main"]["temp"]
+        self.ids.temp.text = f"{round(temp)}°C"
+        desc = weather["weather"][0]["description"]
+        self.ids.desc.text = desc.capitalize()
+        icon = weather["weather"][0]["icon"]
+        self.ids.icon.source = f'https://openweathermap.org/img/wn/{icon}@2x.png'
+
+        
+class ForecastScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.forecast = []
+
+    def get_forecast(self, city):
+        params = {
+            "q": city,
+            "appid": API_KEY,
+        }
+        data = requests.get(FORECAST_URL, params)
+        response = data.json()
+        print(response)
+        return response['list']
+    
+    def show_forecast(self, forecast):
+        #додаємо картки з прогнозом погоди на екран
+        for data in forecast:
+            card = WeatherCard(data)
+            self.ids.weather_list.add_widget(card)
+
+    def back(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'home'
 
 
 class LCloudApp(MDApp):
@@ -50,8 +95,12 @@ class LCloudApp(MDApp):
         Builder.load_file('style.kv')
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Purple"
-
-        return WeatherScreen()
-
+        sm = MDScreenManager()
+        self.weather_screen = WeatherScreen(name='home')
+        self.forecast_screen = ForecastScreen(name='forecast')
+        self.weather_screen.forecast  = self.forecast_screen
+        sm.add_widget(self.weather_screen)
+        sm.add_widget(self.forecast_screen)
+        return sm
 
 LCloudApp().run()
